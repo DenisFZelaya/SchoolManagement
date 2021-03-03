1dd0f67d75f7328635999902ff9d35850a646952
DenisFZelaya <48973732+DenisFZelaya@users.noreply.github.com>
2021-03-03 16:00:15 -0600

    Initial commit

---
+++ SchoolManagement/Models/MetaClases/CoursesMetadata.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace SchoolManagement.Models
+{
+    public class CoursesMetadata
+    {
+        [Required]
+        [StringLength(50)]
+        public string Title { get; set; }
+
+        [Required]
+        [Range(1,8)]
+        public int Credits { get; set; }
+    }
+
+    [MetadataType(typeof(CoursesMetadata))]
+    public partial class Course
+    {
+
+    }
+}
